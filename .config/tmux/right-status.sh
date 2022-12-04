30c8efcde9ec75e101599eb64d99ebde0272c938
srdusr <trevorgray@srdusr.com>
2022-12-04 21:52:27 +0200

    Add right-status script

---
+++ .config/tmux/right-status.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function memory-usage() {
+    if [ "$(which bc)" ]; then
+        # Display used, total, and percentage of memory using the free command.
+        read used total <<< $(free -m | awk '/Mem/{printf $2" "$3}')
+        # Calculate the percentage of memory used with bc.
+        percent=$(bc -l <<< "100 * $total / $used")
+        # Feed the variables into awk and print the values with formating.
+        awk -v u=$used -v t=$total -v p=$percent 'BEGIN {printf "%sMi/%sMi %.1f% ", t, u, p}'
+    fi
+}
+
+function main() {
+    # Comment out any function you do not need. 
+    memory-usage
+}
+
+# Calling the main function which will call the other functions.
+main
