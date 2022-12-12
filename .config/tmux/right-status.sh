ea34e5af7d1b19e95e638c437ea84dc439a69def
srdusr <trevorgray@srdusr.com>
2022-12-12 23:57:01 +0200

    Add memory to right-status script and fixed overall formatting

--- .config/tmux/right-status.sh
+++ .config/tmux/right-status.sh
@@ -7,7 +7,7 @@ function memory-usage() {
         # Calculate the percentage of memory used with bc.
         percent=$(bc -l <<< "100 * $total / $used")
         # Feed the variables into awk and print the values with formating.
-        awk -v u=$used -v t=$total -v p=$percent 'BEGIN {printf "%sMi/%sMi %.1f% ", t, u, p}'
+        awk -v u=$used -v t=$total -v p=$percent 'BEGIN {printf "%s/%s Mem %.1f% ", t, u, p}'
     fi
 }
 
