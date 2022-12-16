18e3249ba8bfea656b02308225684e893d0273fa
srdusr <trevorgray@srdusr.com>
2022-12-16 23:42:45 +0200

    Squashed '.config/nvim/' content from commit f63391e
    git-subtree-dir: .config/nvim
    git-subtree-split: f63391eac31d51b5bab84456b6900fdb3b46a285

---
+++ init.lua
@@ -0,0 +1,138 @@
+--[[
+              ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+              ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+              ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+              ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+              ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+              ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
+              " ------------------------------------------------
+              Author: srdusr
+               Email: graytrevor98@gmail.com
+                 Url: https://github.com/srdusr/nvim.git
+              ------------------------------------------------ "
+--]]
+
+--[[init.]]
+
+-- ========================================================================== --
+-- ==                            DEPENDENCIES                              == --
+-- ========================================================================== --
+
+-- ripgrep    - https://github.com/BurntSushi/ripgrep
+-- fd         - https://github.com/sharkdp/fd
+-- git        - https://git-scm.com/
+-- make       - https://www.gnu.org/software/make/
+-- c compiler - gcc or tcc or zig
+
+-- -------------------------------------------------------------------------- --
+
+-- Initialize config with this one liner in the terminal
+--nvim --headless -c 'call mkdir(stdpath("config"), "p") | exe "edit" stdpath("config") . "/init.lua" | write | quit'
+
+-- See startup time
+--nvim --startuptime startup.log -c exit && tail -100 startup.log
+
+-- Load impatient
+local impatient_ok, impatient = pcall(require, "impatient")
+if impatient_ok then
+	impatient.enable_profile()
+end
+
+-- Check if we have the latest stable version of nvim
+local utils = require("user.utils")
+local expected_ver = "0.8.0"
+local nvim_ver = utils.get_nvim_version()
+
+if nvim_ver ~= expected_ver then
+	local msg = string.format("Unsupported nvim version: expect %s, but got %s instead!", expected_ver, nvim_ver)
+vim.api.nvim_err_writeln(msg)
+	return
+end
+
+-- Schedule reading shadafile to improve the startup time
+vim.opt.shadafile = "NONE"
+vim.schedule(function()
+	vim.opt.shadafile = ""
+	vim.cmd("silent! rsh")
+end)
+
+
+-- Load/reload modules
+local modules = {
+  "user.pack", -- Packer plugin manager
+  "user.opts", -- Options
+  "user.keys", -- Keymaps
+  "user.utils", -- Utilities
+  --"user.mods", -- Modules/functions
+  --"user.deps", -- Plugins
+  --"user.scripts",
+  "plugins.treesitter",
+  "plugins.telescope",
+  "plugins.nvim-tree",
+  "plugins.cmp",
+  "plugins.luasnip",
+  "plugins.colorizer",
+  "plugins.prettier",
+  "plugins.git",
+  "plugins.gitsigns",
+  "plugins.neoscroll",
+  "plugins.lsp",
+  "plugins.autopairs",
+  "plugins.null-ls",
+  "plugins.web-devicons",
+  "plugins.zen-mode",
+  "plugins.colorscheme",
+  "plugins.heirline",
+  --"plugins.dap",
+  --"plugins.toggleterm",
+  --"plugins.floatterm",
+}
+
+-- Refresh module cache
+for k, v in pairs(modules) do
+  package.loaded[v] = nil
+  require(v)
+end
+
+-- Snippets
+vim.g.snippets = "luasnip"
+
+-- Improve speed by disabling some default plugins/modules
+local builtins = {
+	"gzip",
+	"zip",
+	"zipPlugin",
+	"tar",
+	"tarPlugin",
+	"getscript",
+	"getscriptPlugin",
+	"vimball",
+	"vimballPlugin",
+	"2html_plugin",
+	--"matchit",
+	--"matchparen",
+	"logiPat",
+	"rrhelper",
+	"netrw",
+	"netrwPlugin",
+	"netrwSettings",
+	"netrwFileHandlers",
+  "tutor_mode_plugin",
+  "fzf",
+  "spellfile_plugin",
+  "sleuth",
+}
+
+for _, plugin in ipairs(builtins) do
+	vim.g["loaded_" .. plugin] = 1
+end
+vim.g.do_filetype_nvim = 1
+vim.g.did_load_filetypes = 0
+
+--vim.cmd[[
+--if maparg('<C-L>', 'n') ==# ''
+--  nnoremap <silent> <C-L> :set lz!<CR>:nohlsearch<C-R>=has('diff')?'<Bar>diffupdate':''<CR><CR><C-L>:set lz!<CR>
+--endif]]
+--vim.cmd[[autocmd VimEnter * set nolazyredraw lazyredraw]]
+--vim.cmd[[autocmd VimEnter * redraw!]]
+
---
+++ lua/user/utils.lua
@@ -0,0 +1,85 @@
+local fn = vim.fn
+
+local M = {}
+
+function M.executable(name)
+  if fn.executable(name) > 0 then
+    return true
+  end
+
+  return false
+end
+
+--- check whether a feature exists in Nvim
+--- @feat: string
+---   the feature name, like `nvim-0.7` or `unix`.
+--- return: bool
+M.has = function(feat)
+  if fn.has(feat) == 1 then
+    return true
+  end
+
+  return false
+end
+
+--- Create a dir if it does not exist
+function M.may_create_dir(dir)
+  local res = fn.isdirectory(dir)
+
+  if res == 0 then
+    fn.mkdir(dir, "p")
+  end
+end
+
+M['unload_lua_namespace'] = function(prefix)
+  local prefix_with_dot = prefix .. '.'
+  for key, value in pairs(package.loaded) do
+    if key == prefix or key:sub(1, #prefix_with_dot) == prefix_with_dot then
+      package.loaded[key] = nil
+    end
+  end
+end
+
+-- toggle cmp completion
+vim.g.cmp_toggle_flag = false -- initialize
+local normal_buftype = function()
+  return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt"
+end
+M.toggle_completion = function()
+  local ok, cmp = pcall(require, "cmp")
+  if ok then
+    local next_cmp_toggle_flag = not vim.g.cmp_toggle_flag
+    if next_cmp_toggle_flag then
+      print("completion on")
+    else
+      print("completion off")
+    end
+    cmp.setup({
+      enabled = function()
+        vim.g.cmp_toggle_flag = next_cmp_toggle_flag
+        if next_cmp_toggle_flag then
+          return normal_buftype
+        else
+          return next_cmp_toggle_flag
+        end
+      end,
+    })
+  else
+    print("completion not available")
+  end
+end
+
+function M.get_nvim_version()
+  local actual_ver = vim.version()
+
+  local nvim_ver_str = string.format("%d.%d.%d", actual_ver.major, actual_ver.minor, actual_ver.patch)
+  return nvim_ver_str
+end
+
+function M.add_pack(name)
+  local status, error = pcall(vim.cmd, "packadd " .. name)
+
+  return status
+end
+
+return M
