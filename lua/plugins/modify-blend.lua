65cb0bb85d5d581addd6acdbd3a8ce77c761cfa5
srdusr <trevorgray@srdusr.com>
2023-02-01 23:56:16 +0200

    Add anchor = 'NE' option to local win

--- lua/plugins/modify-blend.lua
+++ lua/plugins/modify-blend.lua
@@ -2,9 +2,12 @@ local ui = vim.api.nvim_list_uis()[1]
 
 local bufnr = vim.api.nvim_create_buf(false, true)
 local win = vim.api.nvim_open_win(bufnr, true, {
-  relative = "editor",
+  --relative = "editor",
+  --relative = "win",
+  relative = "cursor",
   width = ui.width,
   height = ui.height,
+  anchor = "NE",
   row = 10,
   col = 10,
   style = "minimal",
