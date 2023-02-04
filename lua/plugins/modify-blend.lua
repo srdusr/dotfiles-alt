f752e3b2d81c099948617669f3ad50da85fc84e7
srdusr <trevorgray@srdusr.com>
2023-02-04 23:51:19 +0200

    Changed relative option = 'cursor' in local win function

--- lua/plugins/modify-blend.lua
+++ lua/plugins/modify-blend.lua
@@ -2,8 +2,8 @@ local ui = vim.api.nvim_list_uis()[1]
 
 local bufnr = vim.api.nvim_create_buf(true, true)
 local win = vim.api.nvim_open_win(bufnr, true, {
-  relative = "editor",
-  --relative = "cursor",
+  --relative = "editor",
+  relative = "cursor",
   width = ui.width,
   height = ui.height,
   anchor = "NE",
@@ -14,30 +14,30 @@ local win = vim.api.nvim_open_win(bufnr, true, {
 })
 
 vim.api.nvim_win_set_option(win, "winblend", 1)
-
-local blend_start = 15
-local offset = 1
-
-CANCEL = false
-local timer = vim.loop.new_timer()
-timer:start(
-  0,
-  50,
-  vim.schedule_wrap(function()
-    blend_start = blend_start + offset
-
-    if blend_start > 90 then
-      offset = -1
-    elseif blend_start < 10 then
-      offset = 1
-    end
-
-    if CANCEL or not vim.api.nvim_win_is_valid(win) then
-      timer:close()
-      timer:stop()
-      return
-    end
-
-    vim.cmd([[highlight NormalFloat blend=]] .. tostring(blend_start))
-  end)
-)
+--
+--local blend_start = 15
+--local offset = 1
+--
+--CANCEL = false
+--local timer = vim.loop.new_timer()
+--timer:start(
+--  0,
+--  50,
+--  vim.schedule_wrap(function()
+--    blend_start = blend_start + offset
+--
+--    if blend_start > 90 then
+--      offset = -1
+--    elseif blend_start < 10 then
+--      offset = 1
+--    end
+--
+--    if CANCEL or not vim.api.nvim_win_is_valid(win) then
+--      timer:close()
+--      timer:stop()
+--      return
+--    end
+--
+--    vim.cmd([[highlight NormalFloat blend=]] .. tostring(blend_start))
+--  end)
+--)
