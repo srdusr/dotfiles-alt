2118a38dc050e0b9cba2f80199b7c0d9798c6828
srdusr <trevorgray@srdusr.com>
2023-02-06 21:51:53 +0200

    Removed unwanted plugins

--- lua/user/pack.lua
+++ lua/user/pack.lua
@@ -13,7 +13,7 @@ if fn.empty(fn.glob(install_path)) > 0 then
 		"https://github.com/wbthomason/packer.nvim",
 		install_path,
 	})
-	print("Installing packer close and reopen Neovim...")
+	print("Installing packer, please close and reopen Neovim...")
 	vim.cmd([[packadd packer.nvim]])
 end
 
@@ -53,7 +53,7 @@ packer.init({
 -- Install plugins here
 return packer.startup(function(use)
   -- Defaults
-	use("wbthomason/packer.nvim") -- Have packer manage itself
+	use("wbthomason/packer.nvim") -- Have packer manage itself (package manager)
   use("nvim-lua/plenary.nvim") -- Useful lua functions used by lots of plugins
 	use("lewis6991/impatient.nvim") -- Faster loading/startup times
 
@@ -65,14 +65,10 @@ return packer.startup(function(use)
   use("williamboman/mason.nvim") -- Package manager to install and manage LSP servers, DAP servers, linters and formatters
   use("williamboman/mason-lspconfig.nvim") -- Bridges mason.nvim with nvim-lspconfig to help use them together
   use("neovim/nvim-lspconfig") -- Collection of LSP configs
-  --use {
-  --  "williamboman/mason.nvim", -- Package manager to install and manage LSP servers, DAP servers, linters and formatters
-  --  "williamboman/mason-lspconfig.nvim", -- Bridges mason.nvim with nvim-lspconfig to help use them together
-  --  "neovim/nvim-lspconfig", -- Collection of LSP configs
-  --}
 
   -- Debugger
 	use("mfussenegger/nvim-dap") -- Debug Adapter Protocol client implementation for Neovim
+	use("rcarriga/nvim-dap-ui") -- UI for nvim-dap
 
 	-- Linters/Formatters
   use({
@@ -84,38 +80,38 @@ return packer.startup(function(use)
 	})
 
 	-- Completion
-  use("hrsh7th/nvim-cmp")
-	use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-path")
-	use("hrsh7th/cmp-cmdline")
-  use("petertriho/cmp-git")
-	use("tamago324/cmp-zsh")
-	use("f3fora/cmp-spell")
-	use("hrsh7th/cmp-calc")
-	use("saadparwaiz1/cmp_luasnip")
-	use("hrsh7th/cmp-nvim-lsp-signature-help")
-	use("onsails/lspkind-nvim")
+  use("hrsh7th/nvim-cmp") -- Completion engine plugin
+	use("hrsh7th/cmp-nvim-lsp") -- Completion source for nvim-lsp
+	use("hrsh7th/cmp-buffer") -- Completion source for content of current buffer
+	use("hrsh7th/cmp-path") -- Completion source for paths
+	use("hrsh7th/cmp-cmdline") -- Completion source for command-line
+  use("petertriho/cmp-git") -- Completion source for git
+	use("tamago324/cmp-zsh") -- Completion source for zsh
+	use("f3fora/cmp-spell") -- Completion source for spell-checking
+	use("hrsh7th/cmp-calc") -- Completion source for math calculation
+	use("saadparwaiz1/cmp_luasnip") -- Completion source for snippets, specifically for luasnip
+	use("hrsh7th/cmp-nvim-lsp-signature-help") -- Completion source for displaying function signatures with the current parameter emphasized
 
 	-- Snippets
 	use("L3MON4D3/LuaSnip") -- Snippet engine
 	use("rafamadriz/friendly-snippets") -- Collection of snippets to use
 
 	-- Git
+	use("tpope/vim-fugitive") -- 
 	use("dinhhuy258/git.nvim") -- For git blame & browse
-  use("kdheepak/lazygit.nvim")
-	use("lewis6991/gitsigns.nvim")
+  use("kdheepak/lazygit.nvim") -- Terminal UI for git commands
+	use("lewis6991/gitsigns.nvim") -- Git decorations
 
   -- File explorer/fuzzy finder
-	use("kyazdani42/nvim-tree.lua")
+	use("kyazdani42/nvim-tree.lua") -- File explorer
 	use('ibhagwan/fzf-lua') -- Fuzzy finder
-	use("nvim-telescope/telescope.nvim")
-	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
-	use("nvim-telescope/telescope-ui-select.nvim")
-	use("nvim-telescope/telescope-media-files.nvim")
-	use("nvim-telescope/telescope-file-browser.nvim")
+	use("nvim-telescope/telescope.nvim") --  Fuzzy finder with lots of features/extendabilities
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- Support fzf syntax/algorithm
+	use("nvim-telescope/telescope-ui-select.nvim") -- 
+	use("nvim-telescope/telescope-media-files.nvim") -- 
+	use("nvim-telescope/telescope-file-browser.nvim") -- 
 	use({ "nvim-telescope/telescope-symbols.nvim", after = "telescope.nvim" }) -- Search emoji(s) and other symbols
-	use("axkirillov/telescope-changed-files")
+	use("axkirillov/telescope-changed-files") -- 
 
 	-- UX
   use({
@@ -125,19 +121,18 @@ return packer.startup(function(use)
     end,
   })
 	use({ "tpope/vim-eunuch", cmd = { "Rename", "Delete" } }) -- Handy unix commands inside Vim (Rename, Move etc.)
-	use("tpope/vim-fugitive")
-	--use("tpope/vim-surround")
-	--use("tpope/vim-obsession")
-	--use("tpope/vim-unimpaired")
-	--use("vimpostor/vim-tpipeline")
-  use("nathom/filetype.nvim")
-	use("myusuf3/numbers.vim")
-	use("windwp/nvim-autopairs")
-	use("numToStr/Comment.nvim")
-	use("akinsho/toggleterm.nvim")
-	use("tweekmonster/startuptime.vim")
+	--use("tpope/vim-surround") -- 
+	--use("tpope/vim-obsession") -- 
+	--use("tpope/vim-unimpaired") -- 
+	--use("vimpostor/vim-tpipeline") -- 
+  use("nathom/filetype.nvim") -- 
+	use("myusuf3/numbers.vim") -- 
+	use("windwp/nvim-autopairs") -- 
+	use("numToStr/Comment.nvim") -- 
+	use("akinsho/toggleterm.nvim") -- 
+	use("tweekmonster/startuptime.vim") -- 
 	use({
-	  "ggandor/leap.nvim",
+	  "ggandor/leap.nvim", -- 
 	  config = function()
       require('leap').add_default_mappings()
 	    --require("leap").set_default_keymaps()
@@ -145,19 +140,19 @@ return packer.startup(function(use)
       --vim.keymap.set('n', '_', '<Plug>(leap-backward)', {})
 	  end,
 	})
-  use({ "ggandor/flit.nvim",
+  use({ "ggandor/flit.nvim", -- 
     config = function()
       require("flit").setup()
     end,
   })
-	use("folke/which-key.nvim")
-	use("folke/zen-mode.nvim")
-	use("romainl/vim-cool")
-  use "antoinemadec/FixCursorHold.nvim"
-  use("airblade/vim-rooter")
-	--use("vim-test/vim-test")
+	use("folke/which-key.nvim") -- 
+	use("folke/zen-mode.nvim") -- 
+	use("romainl/vim-cool") -- 
+  use "antoinemadec/FixCursorHold.nvim" -- 
+  use("airblade/vim-rooter") -- 
+	--use("vim-test/vim-test") -- 
 	--use({
-	--  "rcarriga/vim-ultest",
+	--  "rcarriga/vim-ultest", -- 
 	--  requires = { "vim-test/vim-test" },
 	--  run = ":UpdateRemotePlugins",
 	--  config = function()
@@ -166,39 +161,34 @@ return packer.startup(function(use)
 	--})
 
 	-- Colorschemes
-	use("gruvbox-community/gruvbox")
-	use("srcery-colors/srcery-vim")
-	use("tomasr/molokai")
+	use("bluz71/vim-nightfly-guicolors")
 	use("ayu-theme/ayu-vim")
 	use("joshdick/onedark.vim")
-	use("everblush/everblush.nvim")
-	use("EdenEast/nightfox.nvim")
-	use("bluz71/vim-nightfly-guicolors")
-	use("jacoborus/tender.vim")
-	use("sainnhe/sonokai")
 	use("NTBBloodbath/doom-one.nvim")
 
   -- UI
-	use("kyazdani42/nvim-web-devicons")
-  --use("goolord/alpha-nvim")
+	use("kyazdani42/nvim-web-devicons") -- 
+	use("onsails/lspkind-nvim") -- 
+  --use("goolord/alpha-nvim") -- 
 	use("rcarriga/nvim-notify") -- Notification plugin
-	use("karb94/neoscroll.nvim")
+	use("karb94/neoscroll.nvim") -- Faster/smooth scrolling
 	use("MunifTanjim/prettier.nvim") -- Prettier plugin for Neovim's built-in LSP client
-	use("norcalli/nvim-colorizer.lua")
+	use("norcalli/nvim-colorizer.lua") -- 
   use({ "j-hui/fidget.nvim", -- UI to show nvim-lsp progress
     config = function()
       require("fidget").setup()
     end
   })
-	use("rcarriga/nvim-dap-ui")
-  use { "simrat39/symbols-outline.nvim",
+  use { "simrat39/symbols-outline.nvim", -- 
     config = function()
       require("symbols-outline").setup({
         auto_close = true,
       })
     end
   }
-  use { "kosayoda/nvim-lightbulb", requires = { "antoinemadec/FixCursorHold.nvim" } }
+  use({ "kosayoda/nvim-lightbulb", -- 
+    requires = "antoinemadec/FixCursorHold.nvim",
+  })
   use({
     "SmiteshP/nvim-navic", -- Statusline/Winbar component that uses LSP to show current code context
 		requires = "neovim/nvim-lspconfig",
@@ -210,18 +200,23 @@ return packer.startup(function(use)
   })
 
   -- Language specific tools
-  use "simrat39/rust-tools.nvim"
-  use { "saecki/crates.nvim",
+  use("simrat39/rust-tools.nvim") -- Rust tooling ecosystem
+  use({ "saecki/crates.nvim", -- 
     requires = { "nvim-lua/plenary.nvim" },
     config = function()
       require("crates").setup()
     end,
-  }
-  use({
-    "iamcco/markdown-preview.nvim", -- Markdown Preview
-    run = function() vim.fn["mkdp#util#install"]() end,
   })
-  use {"ellisonleao/glow.nvim", config = function() require("glow").setup() end} -- Markdown Preview
+  --use({
+  --  "iamcco/markdown-preview.nvim", -- Markdown Preview
+  --  run = function() vim.fn["mkdp#util#install"]() end,
+  --})
+  use({
+    "ellisonleao/glow.nvim", -- Markdown Preview
+    config = function()
+    require("glow").setup()
+  end
+  })
 
 --------------------------------------------------
 
