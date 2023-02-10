f9e9b589580af90d9d22156471d663465fa48f59
srdusr <trevorgray@srdusr.com>
2023-02-10 10:51:40 +0200

    Removed Unload lua namespace function

--- lua/user/utils.lua
+++ lua/user/utils.lua
@@ -40,19 +40,6 @@ end
 
 --------------------------------------------------
 
---- Unload lua namespace
-M['unload_lua_namespace'] = function(prefix)
-  local prefix_with_dot = prefix .. '.'
-  for key, value in pairs(package.loaded) do
-    if key == prefix or key:sub(1, #prefix_with_dot) == prefix_with_dot then
-      package.loaded[key] = nil
-    end
-  end
-end
-
-
---------------------------------------------------
-
 --- Toggle cmp completion
 vim.g.cmp_toggle_flag = false -- initialize
 local normal_buftype = function()
