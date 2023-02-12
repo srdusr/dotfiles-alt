565f74484110409e8bdc1269d4ef1953e61d98be
srdusr <trevorgray@srdusr.com>
2023-02-12 16:52:30 +0200

    Add a function to determine if a value of any type is empty

--- lua/user/utils.lua
+++ lua/user/utils.lua
@@ -28,6 +28,20 @@ end
 
 --------------------------------------------------
 
+---Determine if a value of any type is empty
+---@param item any
+---@return boolean?
+function M.empty(item)
+  if not item then return true end
+  local item_type = type(item)
+  if item_type == 'string' then return item == '' end
+  if item_type == 'number' then return item <= 0 end
+  if item_type == 'table' then return vim.tbl_isempty(item) end
+  return item ~= nil
+end
+
+--------------------------------------------------
+
 --- Create a dir if it does not exist
 function M.may_create_dir(dir)
   local res = fn.isdirectory(dir)
