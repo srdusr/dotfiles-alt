73f31dabc03cd963e6e6c22bb53ffe56ad7fc855
srdusr <trevorgray@srdusr.com>
2023-02-18 23:49:18 +0200

    Adjust "find_scripts" hidden value to true

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -299,7 +299,7 @@ end
 
 function M.find_scripts()
   require("telescope.builtin").find_files {
-    hidden = false,
+    hidden = true,
     prompt_title = " Find Notes",
     path_display = { "smart" },
     search_dirs = {
