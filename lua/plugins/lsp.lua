a20adc219f47bab22441bf6dec8f2bbceb28eb26
srdusr <trevorgray@srdusr.com>
2023-02-23 20:41:55 +0200

    Changed signs = true, in vim.lsp.handlers["textDocument/publishDiagnostic"]

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -310,7 +310,7 @@ vim.diagnostic.config({
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
 	underline = false,
 	virtual_text = false,
-	signs = false,
+	signs = true,
 	update_in_insert = false,
 })
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
