a5bd4983f244eade6ff30ede94245406d1fab62c
srdusr <trevorgray@srdusr.com>
2023-04-02 23:54:19 +0200

    Trying to format on save

--- lua/user/mods.lua
+++ lua/user/mods.lua
@@ -27,6 +27,29 @@ end
 
 --------------------------------------------------
 
+
+-- Format on save
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+require("null-ls").setup({
+    -- you can reuse a shared lspconfig on_attach callback here
+    on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                    vim.lsp.buf.formatting_seq_sync()
+                end,
+            })
+        end
+    end,
+})
+
+
+--------------------------------------------------
+
 ---Determine if a value of any type is empty
 ---@param item any
 ---@return boolean?
