d6fbd2c5dd6e0cf732a99665ce8bd1edc92a950d
srdusr <trevorgray@srdusr.com>
2023-04-21 23:05:54 +0200

    Commented out deprecated view.mappings.list

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -65,13 +65,13 @@ nvim_tree.setup({
     width = 30,
     --height = 30,
     side = "left",
-    mappings = {
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb("edit") },
-        { key = "h", cb = tree_cb("close_node") },
-        { key = "v", cb = tree_cb("vsplit") },
-        { key = "u", action = "dir_up" },
-      },
-    },
+    --mappings = {
+    --  list = {
+    --    { key = { "l", "<CR>", "o" }, cb = tree_cb("edit") },
+    --    { key = "h", cb = tree_cb("close_node") },
+    --    { key = "v", cb = tree_cb("vsplit") },
+    --    { key = "u", action = "dir_up" },
+    --  },
+    --},
   },
 })
