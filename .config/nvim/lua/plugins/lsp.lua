e986401a2c97e608ab493ec72b4bb3250ab22a96
srdusr <trevorgray@srdusr.com>
2023-04-23 21:59:02 +0200

    Add sqlls

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -184,6 +184,7 @@ local servers = {
 			},
 		},
 	}),
+	sqlls = {},
   tsserver = { capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
     on_attach = function(client)
         client.server_capabilities.document_formatting = false
