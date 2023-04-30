bd817e6c9fce52ff32d97a87af025c8183cfa78b
srdusr <trevorgray@srdusr.com>
2023-04-30 21:56:31 +0200

    Add highlight groups

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -25,6 +25,9 @@ nvim_tree.setup({
 },
   renderer = {
     root_folder_modifier = ":t",
+    indent_markers = {
+      enable = true,
+    },
     icons = {
       glyphs = {
         default = "",
@@ -75,3 +78,57 @@ nvim_tree.setup({
     --},
   },
 })
+
+-- Highlight Groups
+vim.api.nvim_command("highlight NvimTreeNormal guibg=none")
+--vim.api.nvim_command("highlight NvimTreeSymlink ")
+--vim.api.nvim_command("highlight NvimTreeSymlinkFolderName ")   --(Directory)
+--vim.api.nvim_command("highlight NvimTreeFolderName ")          --(Directory)
+--vim.api.nvim_command("highlight NvimTreeRootFolder ")
+--vim.api.nvim_command("highlight NvimTreeFolderIcon ")
+--vim.api.nvim_command("highlight NvimTreeOpenedFolderIcon ")    --(NvimTreeFolderIcon)
+--vim.api.nvim_command("highlight NvimTreeClosedFolderIcon ")    --(NvimTreeFolderIcon)
+--vim.api.nvim_command("highlight NvimTreeFileIcon ")
+--vim.api.nvim_command("highlight NvimTreeEmptyFolderName ")     --(Directory)
+--vim.api.nvim_command("highlight NvimTreeOpenedFolderName ")    --(Directory)
+--vim.api.nvim_command("highlight NvimTreeExecFile ")
+--vim.api.nvim_command("highlight NvimTreeOpenedFile ")
+--vim.api.nvim_command("highlight NvimTreeModifiedFile ")
+--vim.api.nvim_command("highlight NvimTreeSpecialFile ")
+--vim.api.nvim_command("highlight NvimTreeImageFile ")
+--vim.api.nvim_command("highlight NvimTreeIndentMarker ")
+--vim.api.nvim_command("highlight NvimTreeLspDiagnosticsError ")         --(DiagnosticError)
+--vim.api.nvim_command("highlight NvimTreeLspDiagnosticsWarning ")       --(DiagnosticWarn)
+--vim.api.nvim_command("highlight NvimTreeLspDiagnosticsInformation ")   --(DiagnosticInfo)
+--vim.api.nvim_command("highlight NvimTreeLspDiagnosticsHint ")          --(DiagnosticHint)
+--vim.api.nvim_command("highlight NvimTreeGitDirty ")
+--vim.api.nvim_command("highlight NvimTreeGitStaged ")
+--vim.api.nvim_command("highlight NvimTreeGitMerge ")
+--vim.api.nvim_command("highlight NvimTreeGitRenamed ")
+--vim.api.nvim_command("highlight NvimTreeGitNew ")
+--vim.api.nvim_command("highlight NvimTreeGitDeleted ")
+--vim.api.nvim_command("highlight NvimTreeGitIgnored ")      --(Comment)
+--vim.api.nvim_command("highlight NvimTreeWindowPicker ")
+--vim.api.nvim_command("highlight NvimTreeNormal ")
+--vim.api.nvim_command("highlight NvimTreeEndOfBuffer ")     --(NonText)
+--vim.api.nvim_command("highlight NvimTreeCursorLine ")      --(CursorLine)
+--vim.api.nvim_command("highlight NvimTreeCursorLineNr ")    --(CursorLineNr)
+--vim.api.nvim_command("highlight NvimTreeLineNr ")          --(LineNr)
+--vim.api.nvim_command("highlight NvimTreeWinSeparator ")    --(WinSeparator)
+--vim.api.nvim_command("highlight NvimTreeCursorColumn ")    --(CursorColumn)
+
+
+--vim.api.nvim_command("highlight NvimTreeFileDirty ")       --(NvimTreeGitDirty)
+--vim.api.nvim_command("highlight NvimTreeFileStaged ")      --(NvimTreeGitStaged)
+--vim.api.nvim_command("highlight NvimTreeFileMerge ")       --(NvimTreeGitMerge)
+--vim.api.nvim_command("highlight NvimTreeFileRenamed ")     --(NvimTreeGitRenamed)
+--vim.api.nvim_command("highlight NvimTreeFileNew ")         --(NvimTreeGitNew)
+--vim.api.nvim_command("highlight NvimTreeFileDeleted ")     --(NvimTreeGitDeleted)
+--vim.api.nvim_command("highlight NvimTreeFileIgnored ")     --(NvimTreeGitIgnored)
+
+
+--vim.api.nvim_command("highlight NvimTreeLiveFilterPrefix ")
+--vim.api.nvim_command("highlight NvimTreeLiveFilterValue ")
+
+
+--vim.api.nvim_command("highlight NvimTreeBookmark ")
