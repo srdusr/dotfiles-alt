249fdc9204c5d9223e7910d1751595ed389f2596
srdusr <trevorgray@srdusr.com>
2023-06-28 23:55:17 +0200

    Add OpenEmulatorList

--- lua/user/mods.lua
+++ lua/user/mods.lua
@@ -263,4 +263,19 @@ vim.cmd([[
     --autocmd FocusLost * lua require("user.mods").update_tmux_status()
     --autocmd CmdwinEnter,CmdwinLeave * lua require("user.mods").update_tmux_status()
 --------------------------------------------------
+
+function OpenEmulatorList()
+	local emulatorsBuffer = vim.api.nvim_create_buf(false, true)
+	vim.api.nvim_buf_set_lines(emulatorsBuffer, 0, 0, true, {"Some text"})
+	vim.api.nvim_open_win(
+		emulatorsBuffer,
+		false,
+		{
+			relative='win', row=3, col=3, width=12, height=3
+		}
+	)
+end
+
+vim.api.nvim_create_user_command('OpenEmulators', OpenEmulatorList, {})
+
 return M
