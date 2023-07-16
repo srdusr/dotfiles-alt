933e5894500b3e0101482ed6b168374ae0739158
srdusr <trevorgray@srdusr.com>
2023-07-16 06:13:17 +0200

    Testing, accidental comments not commented

--- win-nvim.ps1
+++ win-nvim.ps1
@@ -29,11 +29,25 @@ if (!(Test-Path $localPacker -PathType Container)) {
 # Set-ExecutionPolicy RemoteSigned
 # Then run the script by using this command in the same existing directory:
 # ./win-nvim.ps1
-curl -o winget-cli.appxbundle https://aka.ms/winget-cli-appxbundle
-powershell Add-AppxPackage -Path  "winget-cli.appxbundle"
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-use `-y` or consider: choco feature enable -n allowGlobalConfirmation
-choco install git
-- Refresh the environment
-Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
-refreshenv
+#curl -o winget-cli.appxbundle https://aka.ms/winget-cli-appxbundle
+#powershell Add-AppxPackage -Path  "winget-cli.appxbundle"
+#Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+#use `-y` or consider: choco feature enable -n allowGlobalConfirmation
+#choco install git
+#- Refresh the environment
+#Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
+#refreshenv
+#
+#
+#here is the error
+#At C:\Users\grayt\scripts\win-nvim.ps1:37 char:2
+#+ - Refresh the environment
+#+ ~
+#Missing expression after unary operator '-'
+#At C:\Users\grayt\scripts\win-nvim.ps1:37 char:3
+#+ - Refresh the environment
+#+ ~~~~~~~~
+#Unexpected token 'Refresh' in expression or statement.
+#  + CategoryInfo : ParserError: (:) [], ParseException
+#  + FullyQualifiedErrorId : MissingExpressionAfterOperator
+
