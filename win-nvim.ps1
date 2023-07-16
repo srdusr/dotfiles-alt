5cf41cf80ec1df5c0df9463ba39b86c00fad2207
srdusr <trevorgray@srdusr.com>
2023-07-16 06:28:04 +0200

    Fixed path errors

--- win-nvim.ps1
+++ win-nvim.ps1
@@ -17,27 +17,24 @@ if (!(Test-Path $dotFilesRoot -PathType Container)) {
 
 # Link NeoVim configuration
 $localConfiguration = Join-Path $env:LOCALAPPDATA "nvim"
-$dotfilesConfiguration = Join-Path (Join-Path $dotFilesRoot ".config") "nvim"
+$dotfilesConfiguration = Join-Path $dotFilesRoot ".config" "nvim"
 
 if (!(Test-Path $localConfiguration -PathType Container)) { 
     cmd /c "mklink /D `"$localConfiguration`" `"$dotfilesConfiguration`"" | Out-Null
 }
 
 # Clone Packer.nvim, if not already present on the system
-$localPacker = Join-Path $env:LOCALAPPDATA "nvim" "pack" "packer" "start" "packer.nvim"
+$packerDirectory = Join-Path $env:LOCALAPPDATA "nvim" "pack" "packer" "opt" "packer.nvim"
 
-if (!(Test-Path $localPacker -PathType Container)) { 
-    git clone https://github.com/wbthomason/packer.nvim $localPacker
+if (!(Test-Path $packerDirectory -PathType Container)) { 
+    git clone https://github.com/wbthomason/packer.nvim $packerDirectory
 }
 
 # To allow script execution, run the following command in PowerShell as an administrator:
 # Set-ExecutionPolicy RemoteSigned
 # Then run the script by using this command in the same existing directory:
 # ./win-nvim.ps1
-# To allow script execution, run the following command in PowerShell as an administrator:
-# Set-ExecutionPolicy RemoteSigned
-# Then run the script by using this command in the same existing directory:
-# ./win-nvim.ps1
+
 #curl -o winget-cli.appxbundle https://aka.ms/winget-cli-appxbundle
 #powershell Add-AppxPackage -Path  "winget-cli.appxbundle"
 #Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
