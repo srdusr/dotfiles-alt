32a71a6f19b45a00b3ea80f5698a2e460769f1e4
srdusr <trevorgray@srdusr.com>
2023-07-16 06:00:36 +0200

    Changed from ssh to https

--- win-nvim.ps1
+++ win-nvim.ps1
@@ -7,7 +7,7 @@ if (!(Get-Command nvim -ErrorAction SilentlyContinue)) {
 $dotFilesRoot = Join-Path $HOME "dotfiles"
 
 if (!(Test-Path $dotFilesRoot -PathType Container)) {
-    git clone git@github.com:srdusr/dotfiles.git $dotFilesRoot
+    git clone https://github.com:srdusr/dotfiles.git $dotFilesRoot
 }
 
 # Link NeoVim configuration
@@ -29,3 +29,11 @@ if (!(Test-Path $localPacker -PathType Container)) {
 # Set-ExecutionPolicy RemoteSigned
 # Then run the script by using this command in the same existing directory:
 # ./win-nvim.ps1
+curl -o winget-cli.appxbundle https://aka.ms/winget-cli-appxbundle
+powershell Add-AppxPackage -Path  "winget-cli.appxbundle"
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+use `-y` or consider: choco feature enable -n allowGlobalConfirmation
+choco install git
+- Refresh the environment
+Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
+refreshenv
