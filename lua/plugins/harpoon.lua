ebbf5e53e79e7fede71de5dbbed1704d4cc5cb26
srdusr <trevorgray@srdusr.com>
2023-07-17 21:30:45 +0200

    Add commented out possible configuration ideas

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -21,3 +21,14 @@ vim.api.nvim_set_keymap("n", "<leader>mh", ":lua require('harpoon.ui').nav_file(
 vim.api.nvim_set_keymap("n", "<leader>mj", ":lua require('harpoon.ui').nav_file(2)<CR>", {})
 vim.api.nvim_set_keymap("n", "<leader>mk", ":lua require('harpoon.ui').nav_file(3)<CR>", {})
 vim.api.nvim_set_keymap("n", "<leader>ml", ":lua require('harpoon.ui').nav_file(4)<CR>", {})
+
+--local mark = require("harpoon.mark")
+--local ui = require("harpoon.ui")
+--
+--vim.keymap.set("n", "<leader>a", mark.add_file)
+--vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+--
+--vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
+--vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
+--vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
+--vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
