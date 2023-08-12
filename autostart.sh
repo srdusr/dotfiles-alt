88cc39a51731276d45d7a2fb79ec7956d753da35
srdusr <trevorgray@srdusr.com>
2023-08-12 23:32:22 +0200

    Made executable and get full path

--- autostart.sh
+++ autostart.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Set the environment variable
+export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u $LOGNAME gnome-session)/environ | tr '\0' '\n' | cut -d= -f2-)
+
 # Load custom dconf settings
-dconf load / <~/.config/dconf-custom/settings.dconf
+/usr/bin/dconf load / <$HOME/.config/dconf-custom/settings.dconf
