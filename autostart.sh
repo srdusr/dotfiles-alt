cdb7ef208bc4ad590cbb9317ea472ccac8d7e3a0
srdusr <trevorgray@srdusr.com>
2023-08-12 23:44:22 +0200

    Use dbus-launch

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Set the environment variable
-export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u $LOGNAME gnome-session)/environ | tr '\0' '\n' | cut -d= -f2-)
-
-# Load custom dconf settings
-/usr/bin/dconf load / <$HOME/.config/dconf-custom/settings.dconf
+# Launch the dconf command with dbus-launch and load custom dconf settings
+dbus-launch /usr/bin/dconf load / <"$HOME"/.config/dconf-custom/settings.dconf
