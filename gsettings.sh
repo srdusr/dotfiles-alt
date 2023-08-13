9fbe2a95bec9c848282689b7a339da8f0deaaff8
srdusr <trevorgray@srdusr.com>
2023-08-13 14:41:49 +0200

    Fixed automatic updates notifications to false

--- gsettings.sh
+++ gsettings.sh
@@ -8,12 +8,13 @@ gsettings set org.gnome.desktop.session idle-delay 0
 gsettings set org.gnome.mutter overlay-key ''
 
 # Disable update notification
-gsettings set org.gnome.software download-updates false
+#gsettings set org.gnome.software download-updates false
+gsettings set com.ubuntu.update-notifier no-show-notifications true
 
 # Custom Keybinding Names
-gsettings set org.gnome.settings-daemon.plugins,media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
 
 # Custom Keybinding 0
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "<Primary><Alt>T"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "<Alt>T"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "scratchpad"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "scratchpad"
