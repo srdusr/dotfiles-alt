443b582eb00d1d3e0f6887071ceff8886ab8a83b
srdusr <trevorgray@srdusr.com>
2023-08-31 10:57:44 +0200

    Normal mode binding 'cd' added

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -130,7 +130,13 @@ require('telescope').setup({
 
         ['<PageUp>'] = actions.results_scrolling_up,
         ['<PageDown>'] = actions.results_scrolling_down,
-
+        ['cd'] = function(prompt_bufnr)
+          local selection = require('telescope.actions.state').get_selected_entry()
+          local dir = vim.fn.fnamemodify(selection.path, ':p:h')
+          require('telescope.actions').close(prompt_bufnr)
+          -- Depending on what you want put `cd`, `lcd`, `tcd`
+          vim.cmd(string.format('silent lcd %s', dir))
+        end,
         ['?'] = actions.which_key,
         --["<C-o>"] = function(prompt_bufnr)
         --	local selection = require("telescope.actions.state").get_selected_entry()
