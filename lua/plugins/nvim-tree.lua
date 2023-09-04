36d11ecbd919f98bb53a1e9406caa3ba102a5ba1
srdusr <trevorgray@srdusr.com>
2023-09-04 22:51:46 +0200

    More configurations for float window

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -72,6 +72,33 @@ local renderer = {
   icons = icons,
 }
 
+local system_open = { cmd = 'zathura' }
+
+local HEIGHT_RATIO = 0.8
+local WIDTH_RATIO = 0.15
+
+local float = {
+  enable = false,
+  open_win_config = function()
+    local screen_w = vim.opt.columns:get()
+    local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+    local window_w = screen_w * WIDTH_RATIO
+    local window_h = screen_h * HEIGHT_RATIO
+    local window_w_int = math.floor(window_w)
+    local window_h_int = math.floor(window_h)
+    local center_x = (screen_w - window_w) / 2
+    local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+    return {
+      border = 'rounded',
+      relative = 'editor',
+      row = center_y,
+      col = center_x,
+      width = window_w_int,
+      height = window_h_int,
+    }
+  end,
+}
+
 local view = {
   cursorline = false,
   hide_root_folder = false,
