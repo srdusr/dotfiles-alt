7a045812d1a80448e1593278b8a7479a48185cdc
srdusr <trevorgray@srdusr.com>
2023-09-04 22:50:10 +0200

    Some changes to the renderer

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -51,6 +51,12 @@ local icons = {
 }
 
 local renderer = {
+  group_empty = true, -- default: true. Compact folders that only contain a single folder into one node in the file tree.
+  highlight_git = false,
+  full_name = false,
+  highlight_opened_files = 'icon', -- "none" (default), "icon", "name" or "all"
+  highlight_modified = 'icon',     -- "none", "name" or "all". Nice and subtle, override the open icon
+  root_folder_label = ':~:s?$?/..?',
   indent_width = 2,
   indent_markers = {
     enable = true,
@@ -64,33 +70,6 @@ local renderer = {
     },
   },
   icons = icons,
-}
-
-local system_open = { cmd = 'zathura' }
-
-local HEIGHT_RATIO = 0.8
-local WIDTH_RATIO = 0.15
-
-local float = {
-  enable = false,
-  open_win_config = function()
-    local screen_w = vim.opt.columns:get()
-    local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
-    local window_w = screen_w * WIDTH_RATIO
-    local window_h = screen_h * HEIGHT_RATIO
-    local window_w_int = math.floor(window_w)
-    local window_h_int = math.floor(window_h)
-    local center_x = (screen_w - window_w) / 2
-    local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
-    return {
-      border = 'rounded',
-      relative = 'editor',
-      row = center_y,
-      col = center_x,
-      width = window_w_int,
-      height = window_h_int,
-    }
-  end,
 }
 
 local view = {
