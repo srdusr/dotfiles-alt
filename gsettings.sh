b7b2755f9b288c2e6ffb3c9e1681f1fc666ace80
srdusr <trevorgray@srdusr.com>
2023-09-15 19:04:30 +0200

    Commented out update-notifier settings

--- gsettings.sh
+++ gsettings.sh
@@ -8,11 +8,12 @@ gsettings set org.gnome.desktop.session idle-delay 0
 gsettings set org.gnome.mutter overlay-key ''
 
 # Disable update notification
-gsettings set org.gnome.software download-updates false
+#gsettings set org.gnome.software download-updates false
 #gsettings set com.ubuntu.update-notifier no-show-notifications true
 #sudo rm /etc/xdg/autostart/upg-notifier-autostart.desktop
-sudo mv /etc/xdg/autostart/update-notifier.desktop /etc/xdg/autostart/update-notifier.desktop.old
-sudo mv /etc/xdg/autostart/gnome-software-service.desktop /etc/xdg/autostart/gnome-software-service.desktop.old
+
+#sudo mv /etc/xdg/autostart/update-notifier.desktop /etc/xdg/autostart/update-notifier.desktop.old
+#sudo mv /etc/xdg/autostart/gnome-software-service.desktop /etc/xdg/autostart/gnome-software-service.desktop.old
 
 # Custom Keybinding Names
 gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
