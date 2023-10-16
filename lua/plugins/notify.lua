24404159dea11b02b0e2c1507fd19a8f20840a1a
srdusr <trevorgray@srdusr.com>
2023-10-16 22:15:57 +0200

    More options for colors

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -8,3 +8,11 @@ require('notify').setup({
     TRACE = '✎',
   },
 })
+
+vim.api.nvim_command('hi default link NotifyERRORBody Normal')
+vim.api.nvim_command('hi default link NotifyWARNBody Normal')
+vim.api.nvim_command('hi default link NotifyINFOBody Normal')
+vim.api.nvim_command('hi default link NotifyDEBUGBody Normal')
+vim.api.nvim_command('hi default link NotifyTRACEBody Normal')
+vim.api.nvim_command('hi default link NotifyLogTime Comment')
+vim.api.nvim_command('hi default link NotifyLogTitle Special')
