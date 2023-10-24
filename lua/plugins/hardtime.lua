23ac4c6f2bc3662a4770b7157374eb9327fc7eda
srdusr <trevorgray@srdusr.com>
2023-10-24 22:16:46 +0200

    Add hardtime.lua

---
+++ lua/plugins/hardtime.lua
@@ -0,0 +1,19 @@
+local hardtime = require('hardtime')
+
+-- Function to toggle the hardtime state and echo a message
+local hardtime_enabled = true
+
+function ToggleHardtime()
+  hardtime.toggle()
+  hardtime_enabled = not hardtime_enabled
+  local message = hardtime_enabled and 'hardtime on' or 'hardtime off'
+  vim.cmd('echo "' .. message .. '"')
+end
+
+hardtime.setup({
+  -- hardtime config here
+})
+
+return {
+  ToggleHardtime = ToggleHardtime,
+}
