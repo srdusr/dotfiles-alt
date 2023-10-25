2f28ed91d9191eaf7d9fb50fa566e7aad232baea
srdusr <trevorgray@srdusr.com>
2023-10-25 13:44:12 +0200

    Also track epub books

--- track-books.sh
+++ track-books.sh
@@ -4,10 +4,10 @@ books_directory="$HOME/documents/books"
 recent_books_file="$HOME/.config/nvim/tmp/recent_books.txt"
 
 inotifywait -m -e CREATE -e OPEN -r "$books_directory" |
-while read -r path action file; do
-    if [[ $file == *.pdf ]]; then
-        echo "$path/$file" >>"$recent_books_file"
-        # Remove duplicates and overwrite the recent_books_file
-        sort -u -o "$recent_books_file" "$recent_books_file"
-    fi
-done
+    while read -r path action file; do
+        if [[ $file == *.pdf || $file == *.epub ]]; then
+            echo "$path/$file" >>"$recent_books_file"
+            # Remove duplicates and overwrite the recent_books_file
+            sort -u -o "$recent_books_file" "$recent_books_file"
+        fi
+    done
