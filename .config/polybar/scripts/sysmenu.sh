fcea4edf4491d2784f0491f19be3b964e5e814b3
srdusr <trevorgray@srdusr.com>
2023-11-19 13:55:48 +0200

    Clean config/moved config to rofi/styles/powermenu.rasi

--- .config/polybar/scripts/sysmenu.sh
+++ .config/polybar/scripts/sysmenu.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# display a power menu to: shutdown, reboot,
+# Display a power menu to: shutdown, reboot,
 # lock, logout, and suspend. This script can be
 # executed by clicking on the polybar powermenu module
 # or with a keyboard shortcut
 
-
-# options to be displayed
+# Options to be displayed
 shutdown=" Shutdown"
 reboot=" Reboot"
 lock=" Lock"
@@ -15,26 +14,29 @@ suspend=" Suspend"
 
 uptime=$(uptime -p | sed -e 's/up //g')
 
-# options passed into variable
+# Options passed into variable
 options="$shutdown\n$reboot\n$lock\n$logout\n$suspend"
 
-chosen="$(echo -e "$options" | rofi -no-lazy-grab -sep -lines 5 -hide-scrollbar true -border 0 -padding 0 -height 2px -width 15 -xoffset -8 -yoffset 28 -location 3 -columns 1 -dmenu -p 'System ' "$uptime")"
+# Specify the path to the Rofi configuration file
+config_file="$HOME/.config/rofi/styles/powermenu.rasi"
+
+# Show Rofi with the specified configuration file
+chosen="$(echo -e "$options" | rofi -no-lazy-grab -sep -config "$config_file" -dmenu -p 'System ' "$uptime")"
 
 case $chosen in
-$shutdown)
-  systemctl poweroff
-  ;;
-$reboot)
-  systemctl reboot
-  ;;
-$lock)
-  betterlockscreen --lock dimblur
-  ;;
-$logout)
-  bspc quit
-  ;;
-$suspend)
-  systemctl suspend
-  ;;
+    $shutdown)
+        systemctl poweroff
+        ;;
+    $reboot)
+        systemctl reboot
+        ;;
+    $lock)
+        betterlockscreen --lock dimblur
+        ;;
+    $logout)
+        bspc quit
+        ;;
+    $suspend)
+        systemctl suspend
+        ;;
 esac
-
