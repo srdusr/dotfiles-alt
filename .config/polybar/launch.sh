de8c95533366c462e12c002e26d58a2688874d94
srdusr <trevorgray@srdusr.com>
2024-01-01 23:52:27 +0200

    Launch polybar left, middle and right

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,13 +4,15 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u "$UID" -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar
-polybar top &
-#polybar bottom &
-#polybar left &
-#polybar top_external &
+polybar left &
+polybar right &
+polybar middle &
+#polybar tray &
+#polybar xwindow &
+#polybar top &
 
 #sleep 5 && xdo raise -N "polybar-bottom_LVDS-1" &
 if [[ $(xrandr -q | grep 'HDMI-1 connected') ]]: then
