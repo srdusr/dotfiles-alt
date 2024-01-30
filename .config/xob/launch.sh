d0ba5d042e791800cb47e2ef1d42649b2b05feb6
srdusr <trevorgray@srdusr.com>
2024-01-30 23:16:55 +0200

    Add xob manage-microphone

--- .config/xob/launch.sh
+++ .config/xob/launch.sh
@@ -3,6 +3,8 @@
 killall -q xob
 pkill -9 manage-volume
 pkill -9 manage-brightness
+pkill -9 manage-microphone
 
 "$HOME"/.config/xob/manage-volume | xob -s default &
 "$HOME"/.config/xob/manage-brightness | xob -s default &
+"$HOME"/.config/xob/manage-microphone | xob -s default &
