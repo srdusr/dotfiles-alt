ffe64ad96e322a072727a2ac83049fb817612fd5
srdusr <trevorgray@srdusr.com>
2024-03-05 23:58:16 +0200

    Add launch.sh

--- .config/xob/launch.sh
+++ .config/xob/launch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 killall -q xob
-pkill -9 manage-volume
-pkill -9 manage-brightness
-pkill -9 manage-microphone
+pkill -f manage-volume
+pkill -f manage-brightness
+pkill -f manage-microphone
 
 "$HOME"/.config/xob/manage-volume | xob -s default &
 "$HOME"/.config/xob/manage-brightness | xob -s default &
