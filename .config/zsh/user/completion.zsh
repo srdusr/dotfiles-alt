506a529ff900d3f8daf257213182a48c4e17a60a
srdusr <trevorgray@srdusr.com>
2024-03-19 23:42:32 +0200

    Removed unset option for FLOW_CONTROL

--- .config/zsh/user/completion.zsh
+++ .config/zsh/user/completion.zsh
@@ -40,7 +40,6 @@ setopt AUTO_LIST            # Automatically list choices on ambiguous completion
 setopt AUTO_PARAM_SLASH     # If completed parameter is a directory, add a trailing slash.
 setopt EXTENDED_GLOB        # Needed for file modification glob modifiers with compinit.
 unsetopt MENU_COMPLETE      # Do not autoselect the first completion entry.
-unsetopt FLOW_CONTROL       # Disable start/stop characters in shell editor.
 
 # Variables
 LS_COLORS=${LS_COLORS:-'di=34:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'}
