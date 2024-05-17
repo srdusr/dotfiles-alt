38453522f3ccc629d0779c2d9cc4867a722a6725
srdusr <trevorgray@srdusr.com>
2024-05-17 22:10:11 +0200

    Alias ls = lsd

--- .config/zsh/user/aliases.zsh
+++ .config/zsh/user/aliases.zsh
@@ -22,11 +22,39 @@ alias ln='ln -i'
 alias rm='function _rm() { echo -e "\033[0;31mrm\033[0m is disabled, use \033[0;32mtrash\033[0m or \033[0;32mdel \033[0m\033[0;33m$1\033[0m"; }; _rm'
 alias del='/bin/rm'
 
-alias ls='ls --color=auto --group-directories-first'
-alias lsd="lsd --group-directories-first"
-alias grep='grep --colour=auto'
-alias egrep='egrep --colour=auto'
-alias fgrep='fgrep --colour=auto'
+alias ls='lsd --color=auto --group-directories-first'
+#alias ls="ls --color=auto --group-directories-first"
+
+# ls variants
+alias l='ls -FAh --group-directories-first'
+alias la='ls -lAFh --group-directories-first'
+alias lt='ls -lFAht --group-directories-first'
+alias lr='ls -RFAh --group-directories-first'
+
+# more ls variants
+alias ldot='ls -ld .* --group-directories-first'
+alias lS='ls -1FASsh --group-directories-first'
+alias lart='ls -1Fcart --group-directories-first'
+alias lrt='ls -1Fcrt --group-directories-first'
+
+# ls with different alphabethical sorting
+#unalias ll
+#ll() { LC_COLLATE=C ls "$@" }
+
+# suffix aliases
+alias -g CP='| xclip -selection clipboard -rmlastnl'
+alias -g LL="| less"
+alias -g CA="| cat -A"
+alias -g KE="2>&1"
+alias -g NE="2>/dev/null"
+alias -g NUL=">/dev/null 2>&1"
+
+alias grep='grep --color=auto --exclude-dir={.git,.svn,.hg}'
+alias egrep='egrep --color=auto --exclude-dir={.git,.svn,.hg}'
+alias egrep='fgrep --color=auto --exclude-dir={.git,.svn,.hg}'
+
+alias gdb='gdb -q'
+alias rust-gdb='rust-gdb -q'
 
 # List upto last 10 visited directories using "d" and quickly cd into any specific one
 alias d="dirs -v | head -10"
@@ -45,10 +73,22 @@ alias 9="cd +9"
 
 alias sudo='sudo ' # zsh: elligible for alias expansion/fix syntax highlight
 alias sedit='sudoedit'
-alias se='sudoedit'
-
+#alias se='sudoedit'
+alias se='sudo -e'
+alias :q='exit'
 alias sc="systemctl"
-alias jc="journalctl xe"
+alias jc="journalctl"
+alias jck="journalctl -k" # Kernel
+alias jce='sudo journalctl -b --priority 0..3' # error
+alias journalctl-error='sudo journalctl -b --priority 0..3'
+alias jcssh="sudo journalctl -u sshd"
+alias tunnel='ssh -fNTL'
+# tty aliases
+if [[ "$TERM" == 'linux' ]]; then
+    alias tmux='/usr/bin/tmux -L linux'
+fi
+alias logout="loginctl kill-user $(whoami)"
+
 #alias suspend='systemctl suspend && betterlockscreen -l' # Suspend(sleep) and lock screen if using systemctl
 alias suspend='systemctl suspend' # Suspend(sleep) and lock screen if using systemctl
 alias hibernate='systemctl hibernate' # Hibernate
@@ -81,6 +121,9 @@ alias src='source ~/.zshrc'
 alias p=proxy
 
 alias cheat='~/.scripts/cheat.sh ~/documents/notes/cheatsheets'
+alias crypto='curl -s rate.sx | head -n -2 | tail -n +10'
+alias todo='glow "$HOME"/media/notes/_TODO.md'
 
 alias android-studio='/opt/android-studio/bin/studio.sh' # android-studio
 alias nomachine='/usr/NX/bin/nxplayer' # nomachine
+alias spotify='LD_PRELOAD=/usr/lib/spotify-adblock.so /bin/spotify %U'
